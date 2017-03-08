1142d2797ff41a95c26cc5c7b23d03656288257d
TenchuHawke <hawke@unrestrainedgames.com>
2017-03-08 12:26:56 -0800

    Created Flush, Straight, and Pair logic

--- Logic.cs
+++ Logic.cs
@@ -1,10 +1,8 @@
 using System.Collections.Generic;
-
+using System.Linq;
 namespace CardGame {
     public static class Logic {
-        public static int IsFlush (Player player, Player shared) {
-            int output, diamonds, clubs, hearts, spades;
-            output = diamonds = clubs = hearts = spades = 0;
+        public static List<Card> Combine (Player player, Player shared) {
             List<Card> combined = new List<Card> ();
             foreach (Card card in player.hand) {
                 combined.Add (card);
@@ -12,6 +10,11 @@ namespace CardGame {
             foreach (Card card in shared.hand) {
                 combined.Add (card);
             }
+            return combined;
+        }
+        public static int IsFlush (List<Card> combined) {
+            int output, diamonds, clubs, hearts, spades;
+            output = diamonds = clubs = hearts = spades = 0;
             foreach (Card card in combined) {
                 switch (card.suit) {
                     case "clubs":
@@ -45,8 +48,55 @@ namespace CardGame {
             } else if (hearts >= 5) {
                 output += 8;
             }
-
             return output;
+        }
+        public static Card IsStraight (List<Card> combined) {
+            List<Card> SortedList = combined.OrderBy (o => o.val).ToList ();
+            if (SortedList.Count < 5) {
+                return null;
+            }
+            for (int i = 0; i < SortedList.Count - 4; i++) {
+                if (SortedList[0].val == 1) {
+                    if (SortedList[i + 1].val == SortedList[i].val + 1) {
+                        if (SortedList[i + 3].val == SortedList[i].val + 2) {
+                            if (SortedList[i + 4].val == SortedList[i].val + 3) {
+                                if (SortedList[i + 4].val == 13) {
+                                    return SortedList[0];
+                                }
+                            }
+                        }
+                    }
+                }
+                if (SortedList[i + 1].val == SortedList[0].val + 1) {
+                    if (SortedList[i + 2].val == SortedList[0].val + 2) {
+                        if (SortedList[i + 3].val == SortedList[0].val + 3) {
+                            if (SortedList[i + 4].val == SortedList[0].val + 4) {
+                                return SortedList[i + 4];
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        public static Dictionary<Dictionary<int, int>, List<Card>> FindPairs (List<Card> combined) {
+            var Pairs = from obj in combined group obj by obj.val into g select new { Name = g.Key, Duplicatecount = g.Count() };
+            List <Card> Matches = new List <Card>();
+            foreach(var m in Pairs){
+                if (m.Duplicatecount>1){
+                    foreach (Card card in combined){
+                        if (card.val == (int)m.Name){
+                            Matches.Add(card);
+                        }
+                    }
+                }
+            }
+            Dictionary <int, int> PairCount = Pairs as Dictionary <int, int>;
+            Dictionary <Dictionary<int, int>, List<Card>> PairsOut = new Dictionary <Dictionary <int, int>, List<Card>>()
+            {
+                {PairCount, Matches}
+            };
+            return PairsOut;
         }
     }
 }
