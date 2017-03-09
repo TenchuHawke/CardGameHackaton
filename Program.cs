9cbfecc70968f6c2fc728d24ceb85b90e3a86c16
TenchuHawke <hawke@unrestrainedgames.com>
2017-03-08 16:48:50 -0800

    Minimum VIable Product

--- Program.cs
+++ Program.cs
@@ -3,117 +3,120 @@ namespace CardGame {
     class Program {
         static void Main (string[] args) {
             // Commands.Test();
-            // sets color for table.
+            // // sets color for table.
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.Clear ();
             // creates a player object and tests that object.
             // SET UP - creates table and add player
-            Table table = new Table();
-            table.resetCommon();
+            Table table = new Table ();
+            table.resetCommon ();
             Player Sam = new Player ("Sam");
-            table.playerList.Add(Sam);
+            table.playerList.Add (Sam);
             Player Bill = new Player ("Bill");
-            table.playerList.Add(Bill);
+            table.playerList.Add (Bill);
             Player Sarah = new Player ("Sarah");
-            table.playerList.Add(Sarah);
+            table.playerList.Add (Sarah);
             bool gameOn = true;
-            while (gameOn){
-
-            
-            table.myDeck.shuffle();
-
-            // System.Console.WriteLine (Player1.playerName);
-            // System.Console.WriteLine (Player1.money);
-            // creates a new deck and tests that object.
-            // Deck myDeck = new Deck ();
-            Console.WriteLine (table.myDeck.ToString ());
-            // shuffles the deck.
-            // table.myDeck.shuffle ();
-            // prints each card in the approperiate color.
-            // foreach (Card card in table.myDeck.cards) {
-            //     if (card.color == "red") {
-            //         Console.ForegroundColor = ConsoleColor.Red;
-            //         System.Console.WriteLine (card.ToString ());
-            //         // Console.ForegroundColor = ConsoleColor.Green;
-            //     } else if (card.color == "black") {
-            //         Console.ForegroundColor = ConsoleColor.Black;
-            //         // Console.BackgroundColor = ConsoleColor.White;
-            //         System.Console.WriteLine (card.ToString ());
-            //         // Console.ForegroundColor = ConsoleColor.Green;
-            //     } else {
-            //         Console.ForegroundColor = ConsoleColor.White;
-            //         System.Console.WriteLine (card.ToString ());
-            //         Console.ForegroundColor = ConsoleColor.Green;
-            //     }
-            // }
-            // pauses before closing.
-            // Commands.Pause ();
-
-            Player temp = table.playerList[0];
-            table.playerList.RemoveAt(0);
-            table.playerList.Add(temp);
-            temp = null;
-            table.anteUp();
-            var currentBetValue = table.currentBetValue;
-            // deal hand of 2 cards.
-            for(var i = 1; i <= 2; i++){
-                for(var j = 0; j < table.playerList.Count; j++){
-                    table.playerList[j].hand.Add(table.myDeck.deal());
+            while (gameOn) {
+                table.myDeck.shuffle ();
+                // System.Console.WriteLine (Player1.playerName);
+                // System.Console.WriteLine (Player1.money);
+                // creates a new deck and tests that object.
+                // Deck myDeck = new Deck ();
+                // Console.WriteLine (table.myDeck.ToString ());
+                // shuffles the deck.
+                // table.myDeck.shuffle ();
+                // prints each card in the approperiate color.
+                // foreach (Card card in table.myDeck.cards) {
+                //     if (card.color == "red") {
+                //         Console.ForegroundColor = ConsoleColor.Red;
+                //         System.Console.WriteLine (card.ToString ());
+                //         // Console.ForegroundColor = ConsoleColor.Green;
+                //     } else if (card.color == "black") {
+                //         Console.ForegroundColor = ConsoleColor.Black;
+                //         // Console.BackgroundColor = ConsoleColor.White;
+                //         System.Console.WriteLine (card.ToString ());
+                //         // Console.ForegroundColor = ConsoleColor.Green;
+                //     } else {
+                //         Console.ForegroundColor = ConsoleColor.White;
+                //         System.Console.WriteLine (card.ToString ());
+                //         Console.ForegroundColor = ConsoleColor.Green;
+                //     }
+                // }
+                // pauses before closing.
+                // Commands.Pause ();
+                Player temp = table.playerList[0];
+                table.playerList.RemoveAt (0);
+                table.playerList.Add (temp);
+                temp = null;
+                table.anteUp ();
+                var currentBetValue = table.currentBetValue;
+                // deal hand of 2 cards.
+                for (var i = 1; i <= 2; i++) {
+                    for (var j = 0; j < table.playerList.Count; j++) {
+                        table.playerList[j].hand.Add (table.myDeck.deal ());
+                    }
                 }
-            }
-            //bet
-            for(var i = 0; i < table.playerList.Count; i++){
-                table.bet(i,10);
-            }
-            Commands.Pause();
-            //burn 1 card
-            table.myDeck.burn();
-            // deal 3 cards to common
-            for(var i = 1; i <= 3; i++){   
-                table.common.hand.Add(table.myDeck.deal());
+                //bet
+                for (var i = 0; i < table.playerList.Count; i++) {
+                    table.bet (i, 10);
+                    Commands.Pause ();
+                }
+                //burn 1 card
+                table.myDeck.burn ();
+                Commands.Pause ();
+                // deal 3 cards to common
+                for (var i = 1; i <= 3; i++) {
+                    table.common.hand.Add (table.myDeck.deal ());
                     // System.Console.WriteLine(card.ToString());
-            }
-            table.displayCommonCards();
-            //bet
-            for(var i = 0; i < table.playerList.Count; i++){
-                table.bet(i,10);
-            }
-            Commands.Pause();
-            //burn 1 card
-            table.myDeck.burn();
-            // deal 1 card to common
-            table.common.hand.Add(table.myDeck.deal());
-            table.displayCommonCards();
-            //bet
-           for(var i = 0; i < table.playerList.Count; i++){
-                table.bet(i,10);
-            }
-           Commands.Pause();
-            //burn 1 card
-            table.myDeck.burn();
-            // deal 1 card to common
-            table.common.hand.Add(table.myDeck.deal());
-            table.displayCommonCards();
-            //bet
-           for(var i = 0; i < table.playerList.Count; i++){
-                table.bet(i,50);
-            }
-            Commands.Pause();
-
-            System.Console.WriteLine("Pot value is {0}",table.potValue);
-            System.Console.WriteLine("List of 5 cards");
-            table.displayCommonCards();
-            
-            for(var i = 0; i < table.playerList.Count; i++){
-                System.Console.WriteLine(table.playerList[i].playerName);
-                table.displayPlayerCards(i);
-            }
-            System.Console.WriteLine("Quit to end");
-            string Input = Console.ReadLine();
-            if(Input == "Quit"){
+                }
+                table.displayCommonCards ();
+                //bet
+                for (var i = 0; i < table.playerList.Count; i++) {
+                    table.bet (i, 10);
+                    Commands.Pause ();
+                }
+                //burn 1 card
+                table.myDeck.burn ();
+                Commands.Pause ();
+                // deal 1 card to common
+                table.common.hand.Add (table.myDeck.deal ());
+                table.displayCommonCards ();
+                //bet
+                for (var i = 0; i < table.playerList.Count; i++) {
+                    table.bet (i, 10);
+                    Commands.Pause ();
+                }
+                //burn 1 card
+                table.myDeck.burn ();
+                Commands.Pause ();
+                // deal 1 card to common
+                table.common.hand.Add (table.myDeck.deal ());
+                table.displayCommonCards ();
+                //bet
+                for (var i = 0; i < table.playerList.Count; i++) {
+                    table.bet (i, 50);
+                    Commands.Pause ();
+                }
+                System.Console.WriteLine ("Pot value is {0}", table.potValue);
+                System.Console.WriteLine ("List of 5 cards");
+                table.displayCommonCards ();
+                Commands.Pause ();
+                for (var i = 0; i < table.playerList.Count; i++) {
+                    System.Console.WriteLine (table.playerList[i].playerName);
+                    string output = "";
+                    output += Logic.ValueHand (Logic.Combine (table.playerList[i], table.common));
+                    System.Console.WriteLine ("Hand Value: " + output);
+                    table.displayPlayerCards (i);
+                    Commands.Pause ();
+                }
+                System.Console.WriteLine ("Quit to end");
+                string Input = Console.ReadLine ();
+                if (Input == "Quit") {
                     gameOn = false;
+                }
+                table.reset ();
             }
         }
-    }
     }
 }
--- Table.cs
+++ Table.cs
@@ -1,79 +1,82 @@
 using System.Collections.Generic;
-
-namespace CardGame{
-    public class Table{
-        public List<Player> playerList = new List<Player>();
-        
+namespace CardGame {
+    public class Table {
+        public List<Player> playerList = new List<Player> ();
         // public List<Player> bettingOrderList = new List<Player>(playerList);
         public int potValue;
         public int currentBetValue;
-        public Deck myDeck = new Deck();
-
-        public Player common = new Player();
-        public void resetCommon(){
+        public Deck myDeck = new Deck ();
+        public Player common = new Player ();
+        public void resetCommon () {
             common.money = 0;
         }
-        public int SmallAnte(int playerIndex, int ante){
-            System.Console.WriteLine("ANTE PLEASE");
+        public int SmallAnte (int playerIndex, int ante) {
+            System.Console.WriteLine ("ANTE PLEASE");
             currentBetValue = ante;
             potValue += ante;
             playerList[playerIndex].money -= ante;
             return ante;
         }
-        public int BigAnte(int playerIndex, int ante){
-            System.Console.WriteLine("ANTE PLEASE");
+        public int BigAnte (int playerIndex, int ante) {
+            System.Console.WriteLine ("ANTE PLEASE");
             currentBetValue = ante;
             potValue += ante;
             playerList[playerIndex].money -= ante;
             return ante;
         }
-        public void anteUp(){
-            this.SmallAnte(0,2);
-            this.BigAnte(1,4);
-            this.BigAnte(2,4);
-            this.SmallAnte(0,2);
+        public void anteUp () {
+            this.SmallAnte (0, 2);
+            this.BigAnte (1, 4);
+            this.BigAnte (2, 4);
+            this.SmallAnte (0, 2);
         }
-        public int bet(int playerIndex, int bet){
-            System.Console.WriteLine("{0} turn to bet!",playerList[playerIndex].playerName);
+        public int bet (int playerIndex, int bet) {
+            System.Console.WriteLine ("{0} turn to bet!", playerList[playerIndex].playerName);
             potValue += bet;
             playerList[playerIndex].money -= bet;
-            System.Console.WriteLine("{0} IS BETTING {1}!",playerList[playerIndex].playerName,bet);
+            System.Console.WriteLine ("{0} IS BETTING {1}!", playerList[playerIndex].playerName, bet);
             return bet;
         }
-        public void displayCommonCards(){
+        public void displayCommonCards () {
             foreach (Card card in common.hand) {
                 if (card.color == "red") {
                     System.Console.ForegroundColor = System.ConsoleColor.Red;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
-                } 
-                else if (card.color == "black") {
+                } else if (card.color == "black") {
                     System.Console.ForegroundColor = System.ConsoleColor.Black;
                     // System.Console.BackgroundColor = System.ConsoleColor.White;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
-                } 
-                else {
+                } else {
                     System.Console.ForegroundColor = System.ConsoleColor.White;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
                 }
             }
         }
-        public void displayPlayerCards(int playerIndex){
+        public void reset () {
+            foreach (Player player in this.playerList) {
+                player.hand.Clear ();
+            }
+            myDeck.reset ();
+            myDeck.shuffle ();
+            common.hand.Clear ();
+            potValue = 0;
+            currentBetValue = 0;
+        }
+        public void displayPlayerCards (int playerIndex) {
             foreach (Card card in playerList[playerIndex].hand) {
                 if (card.color == "red") {
                     System.Console.ForegroundColor = System.ConsoleColor.Red;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
-                } 
-                else if (card.color == "black") {
+                } else if (card.color == "black") {
                     System.Console.ForegroundColor = System.ConsoleColor.Black;
                     // System.Console.BackgroundColor = System.ConsoleColor.White;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
-                } 
-                else {
+                } else {
                     System.Console.ForegroundColor = System.ConsoleColor.White;
                     System.Console.WriteLine (card.ToString ());
                     System.Console.ForegroundColor = System.ConsoleColor.Green;
